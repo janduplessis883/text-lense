4c5db609e5efe1f61a2d140c47857e1cedcc2d24
Jan du Plessis <drjanduplessis@icloud.com>
2025-05-15 18:16:19 +0100

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+# list dependencies from file
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(
+    name='autonote',
+    description="Code snippet manager for Jupyter Notebook & Notebook enhancements + data science helper functions",
+    packages=find_packages(),  # It will find all packages in your directory
+    install_requires=requirements  # This is the key line to install dependencies
+)
